866f8688adb345c616a1ac0adb35796cadcecb88
gkiryaziev <linko@ukr.net>
2016-05-10 19:45:38 +0300

    [FIX] fix import path

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
 
-	"./models"
+	"github.com/gkiryaziev/go-simple-http-server/models"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
